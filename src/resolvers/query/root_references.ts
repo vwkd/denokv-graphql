0a946e4a6b5d402f7c23e14e81556097dfee6c0f
vwkd <33468089+vwkd@users.noreply.github.com>
2023-06-28 23:24:42 +0200

    query: finish fetching keys individually

--- src/resolvers/query/root_references.ts
+++ src/resolvers/query/root_references.ts
@@ -82,10 +82,10 @@ export function createRootReferencesResolver(
 
     context.checks = [];
 
-    // todo: how to paginate over `[tableName, "*", "id"]`?
     if (first) {
       const keysPrefix = [tableName];
 
+      // todo: can't paginate only over `[tableName, "*", "id"]`, limit assuming database is correct, error check if corrupt database with more or less keys, see https://github.com/denoland/deploy_feedback/issues/415
       // note: get one more element to see if has next
       const entries = db.list({ prefix: keysPrefix }, {
         // note: since paginates over column keyspace
@@ -94,24 +94,59 @@ export function createRootReferencesResolver(
       });
 
       let rows = [];
-      let currentId = "";
-      let currentIndex = -1;
-      let cursors = {};
 
       for await (const { key, value, versionstamp } of entries) {
-        if (
-          !(key.length == 3 && typeof key.at(-2) == "string" &&
-            typeof key.at(-1) == "string")
-        ) {
+        if (!(key.length == 3 || key.length == 4)) {
           throw new DatabaseCorruption(
-            `Expected table '${tableName}' to have two-level keys of strings`,
+            `Expected table '${tableName}' to have three-level or four-level keys`,
           );
         }
 
-        if (!(key.at(-2).length > 0 && key.at(-1).length > 0)) {
+        const rowId = key.at(1);
+        const columnName = key.at(2);
+
+        if (typeof rowId != "string") {
           throw new DatabaseCorruption(
-            `Expected table '${tableName}' to have keys of non-empty strings`,
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of string`,
           );
+        }
+
+        if (rowId.length == 0) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of non-empty string`,
+          );
+        }
+
+        if (columnName && typeof columnName != "string") {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of string`,
+          );
+        }
+
+        if (columnName && columnName.length == 0) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of non-empty string`,
+          );
+        }
+
+        if (!columnNames.includes(columnName)) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' key '${columnName}' to be column name`,
+          );
+        }
+
+        if (columnName != "id") {
+          continue;
+        }
+
+        if (columnName == "id" && value !== rowId) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column 'id' to be equal to row id`,
+          );
+        }
+
+        if (rows.length >= first) {
+          break;
         }
 
         context.checks.push({ key, versionstamp });
@@ -119,54 +154,7 @@ export function createRootReferencesResolver(
         // note: always non-empty string
         const cursor = entries.cursor;
 
-        const id = key.at(-2) as string;
-        const columnName = key.at(-1) as string;
-
-        if (!columnNames.includes(columnName)) {
-          throw new DatabaseCorruption(
-            `Expected table '${tableName}' row '${id}' second-level key to be column name`,
-          );
-        }
-
-        if (columnName == "id" && value !== id) {
-          throw new DatabaseCorruption(
-            `Expected table '${tableName}' row '${id}' column 'id' to be equal to row id`,
-          );
-        }
-
-        // next batch of keys of a row
-        // note: assumes Deno KV stores sub-keys sorted
-        if (id != currentId) {
-          // validate previous row is complete
-          // note: last row might be incomplete
-          const lastIndex = currentIndex - 1;
-          if (lastIndex >= 0) {
-            // todo: what about optional columns that aren't set? should allow less than?
-            if (Object.keys(rows[lastIndex]).length != columnNumber) {
-              throw new DatabaseCorruption(
-                `Expected table '${tableName}' row '${currentId}' to have '${columnNumber}' second-level keys for column`,
-              );
-            }
-          }
-
-          currentIndex += 1;
-          currentId = id;
-          rows[currentIndex] = {};
-        }
-
-        rows[currentIndex][columnName] = value;
-
-        // note: overwrites constantly, last one survives
-        cursors[id] = cursor;
-      }
-
-      // remove extra element if it exists
-      if (rows.length == first + 1) {
-        if (Object.keys(rows.at(-1)).length < columnNumber) {
-          rows = rows.slice(0, -1);
-        } else {
-          throw new Error("should be unreachable");
-        }
+        rows.push({ id: rowId, cursor });
       }
 
       // note: cursor of next item if it exists, otherwise empty string if no further items, never `undefined`!
@@ -176,13 +164,15 @@ export function createRootReferencesResolver(
 
       // todo: what if no entries?
 
-      for (const row of rows) {
-        const id = row.id;
+      for (const { id, cursor } of rows) {
         // todo: what to use as versionstamp for whole row? there is no one since can update each column independently, might update leaf or reference table...
         const versionstamp = "foo";
 
+        const row = {
+          id,
+        };
+
         const node = { id, value: row, versionstamp };
-        const cursor = cursors[id];
 
         edges.push({
           node,
@@ -207,7 +197,121 @@ export function createRootReferencesResolver(
 
       return connection;
     } else if (last) {
-      // todo:
+      const keysPrefix = [tableName];
+
+      // todo: can't paginate only over `[tableName, "*", "id"]`, limit assuming database is correct, error check if corrupt database with more or less keys, see https://github.com/denoland/deploy_feedback/issues/415
+      // note: get one more element to see if has next
+      // note: `reverse` to go backwards instead of forwards, then reverse array to end up with forward order
+      const entries = db.list({ prefix: keysPrefix }, {
+        // note: since paginates over column keyspace
+        limit: (last * columnNumber) + 1,
+        cursor: before,
+        reverse: true,
+      });
+
+      let rows = [];
+
+      for await (const { key, value, versionstamp } of entries) {
+        if (!(key.length == 3 || key.length == 4)) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' to have three-level or four-level keys`,
+          );
+        }
+
+        const rowId = key.at(1);
+        const columnName = key.at(2);
+
+        if (typeof rowId != "string") {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of string`,
+          );
+        }
+
+        if (rowId.length == 0) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of non-empty string`,
+          );
+        }
+
+        if (columnName && typeof columnName != "string") {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of string`,
+          );
+        }
+
+        if (columnName && columnName.length == 0) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column '${columnName}' to have key of non-empty string`,
+          );
+        }
+
+        if (!columnNames.includes(columnName)) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' key '${columnName}' to be column name`,
+          );
+        }
+
+        if (columnName != "id") {
+          continue;
+        }
+
+        if (columnName == "id" && value !== rowId) {
+          throw new DatabaseCorruption(
+            `Expected table '${tableName}' row '${rowId}' column 'id' to be equal to row id`,
+          );
+        }
+
+        if (rows.length >= last) {
+          break;
+        }
+
+        context.checks.push({ key, versionstamp });
+
+        // note: always non-empty string
+        const cursor = entries.cursor;
+
+        rows.unshift({ id: rowId, cursor });
+      }
+
+      // note: cursor of next item if it exists, otherwise empty string if no further items, never `undefined`!
+      const endCursorNext = entries.cursor;
+
+      let edges = [];
+
+      // todo: what if no entries?
+
+      for (const { id, cursor } of rows) {
+        // todo: what to use as versionstamp for whole row? there is no one since can update each column independently, might update leaf or reference table...
+        const versionstamp = "foo";
+
+        const row = {
+          id,
+        };
+
+        const node = { id, value: row, versionstamp };
+
+        edges.push({
+          node,
+          cursor,
+        });
+      }
+
+      // todo: handle optional, return null if optional, else throw?
+
+      const pageInfo = {
+        startCursor: edges.at(0)?.cursor,
+        endCursor: edges.at(-1)?.cursor,
+        // note: currently mistakenly set to `true` if passes bogus cursor that passes validation in `db.list`
+        hasNextPage: !!before,
+        hasPreviousPage: !!endCursorNext,
+      };
+
+      const connection = {
+        edges,
+        pageInfo,
+      };
+
+      return connection;
     } else {
       throw new Error(`should be unreachable`);
     }
