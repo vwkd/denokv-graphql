902d007d99761bcd8adbc3e6b58e74f0ab128bf4
vwkd <33468089+vwkd@users.noreply.github.com>
2023-07-15 18:53:45 +0200

    mod: fix export, export errors, fix formatting

--- mod.ts
+++ mod.ts
@@ -2,17 +2,22 @@
  * GraphQL bindings for Deno KV
  *
  * ## Getting started
- * 
+ *
  * ```js
  * import { buildSchema } from "https://deno.land/x/denokv_graphql/mod.ts";
- * 
+ *
  * const schema = buildSchema(db, schemaSource);
  * ```
- * 
+ *
  * Check out the [examples](./examples).
  *
  * @module
  */
  
- export { buildSchema } from "src/main.ts";
- 
+ export { buildSchema } from "./src/main.ts";
+ export {
+  DatabaseCorruption,
+  InvalidSchema,
+  InvalidInput,
+  ConcurrentChange,
+} from "./src/utils.ts";
